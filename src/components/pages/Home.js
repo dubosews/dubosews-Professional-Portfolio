04badb920f622a4dd29b44f21bdff0a25dae6293
Wesley DuBose <wsd10205@gmail.com>
2022-07-08 23:08:34 -0400

    added homepage

---
+++ src/components/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div className="homeContentCard">
+      
+      <div className="homeWelc centerLR"> WELCOME TO </div>
+      <div className="homeTitle centerLR">Wesley DuBose's Portfolio</div>
+      <div className="homeDescContainer centerLR">
+        <div className="homeDescDev"> - Front End Development - </div>
+        <div className="homeDescDev"> - Back End Development - </div>
+      </div>
+      
+    </div>
+  );
+}
