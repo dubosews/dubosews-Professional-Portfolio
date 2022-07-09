e49945f423d8a08d6614834213f5df828dd9d817
Wesley DuBose <wsd10205@gmail.com>
2022-07-09 15:51:03 -0400

    finally added index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/nav.css';
+import './css/app.css';
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
