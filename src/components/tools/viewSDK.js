5ef11ff0909a50bc619f330d310710c13b60eb96
Wesley DuBose <wsd10205@gmail.com>
2022-07-09 23:29:10 -0400

    Update viewSDK.js

--- src/components/tools/viewSDK.js
+++ src/components/tools/viewSDK.js
@@ -1,23 +1,14 @@
-/*
-Copyright 2020 Adobe
-All Rights Reserved.
-
-NOTICE: Adobe permits you to use, modify, and distribute this file in
-accordance with the terms of the Adobe license agreement accompanying
-it. If you have received this file from a source other than Adobe,
-then your use, modification, or distribution of it requires the prior
-written permission of Adobe.
-*/
 import ResumeFile from '../../docs/dubosewsResume.pdf';
-import CertificateFile from '../../docs/dubosewsUNCCertificate.pdf'
+import CertificateFile from '../../docs/dubosewsUNCCertificate.pdf';
+import OutwardCertificate from '../../docs/dubosewsOutwardBound.pdf';
 
 class ViewSDKClient {
+
     constructor() {
         this.readyPromise = new Promise((resolve) => {
             if (window.AdobeDC) {
                 resolve();
             } else {
-                /* Wait for Adobe Document Services PDF Embed API to be ready */
                 document.addEventListener("adobe_dc_view_sdk.ready", () => {
                     resolve();
                 });
@@ -30,41 +21,23 @@ class ViewSDKClient {
         return this.readyPromise;
     }
 
-    previewFile(divId, viewerConfig) {
+    previewResume(divId, viewerConfig) {
         const config = {
-            /* Pass your registered client id */
-            clientId: "8c0cd670273d451cbc9b351b11d22318",
+            clientId: "8de373cceb0744de81c8ce0ab7c9b434",
         };
-        if (divId) { /* Optional only for Light Box embed mode */
-            /* Pass the div id in which PDF should be rendered */
+        if (divId) { 
             config.divId = divId;
         }
-        /* Initialize the AdobeDC View object */
         this.adobeDCView = new window.AdobeDC.View(config);
 
-        /* Invoke the file preview API on Adobe DC View object */
         const previewFilePromise = this.adobeDCView.previewFile({
-            /* Pass information on how to access the file */
             content: {
-                /* Location of file where it is hosted */
                 location: {
                     url: ResumeFile,
-                    /*
-                    If the file URL requires some additional headers, then it can be passed as follows:-
-                    headers: [
-                        {
-                            key: "<HEADER_KEY>",
-                            value: "<HEADER_VALUE>",
-                        }
-                    ]
-                    */
                 },
             },
-            /* Pass meta data of file */
             metaData: {
-                /* file name */
                 fileName: "dubosewsResume.pdf",
-                /* file ID */
                 id: "6d07d124-ac85-43b3-a867-36930f502ac6",
             },
         }, {defaultViewMode: "FIT_WIDTH", dockPageControls: false}, viewerConfig);
@@ -72,41 +45,47 @@ class ViewSDKClient {
         return previewFilePromise;
     }
 
-    previewFile2(divId, viewerConfig) {
+    previewCertificate(divId, viewerConfig) {
         const config = {
-            /* Pass your registered client id */
-            clientId: "8c0cd670273d451cbc9b351b11d22318",
+            clientId: "8de373cceb0744de81c8ce0ab7c9b434",
         };
-        if (divId) { /* Optional only for Light Box embed mode */
-            /* Pass the div id in which PDF should be rendered */
+        if (divId) {
             config.divId = divId;
         }
-        /* Initialize the AdobeDC View object */
         this.adobeDCView = new window.AdobeDC.View(config);
 
-        /* Invoke the file preview API on Adobe DC View object */
         const previewFilePromise = this.adobeDCView.previewFile({
-            /* Pass information on how to access the file */
             content: {
-                /* Location of file where it is hosted */
                 location: {
                     url: CertificateFile,
-                    /*
-                    If the file URL requires some additional headers, then it can be passed as follows:-
-                    headers: [
-                        {
-                            key: "<HEADER_KEY>",
-                            value: "<HEADER_VALUE>",
-                        }
-                    ]
-                    */
                 },
             },
-            /* Pass meta data of file */
             metaData: {
-                /* file name */
                 fileName: "dubosewsUNCCertificate.pdf",
-                /* file ID */
+                id: "6d07d124-ac85-43b3-a867-36930f502ac6",
+            },
+        }, {defaultViewMode: "fit", dockPageControls: false}, viewerConfig);
+
+        return previewFilePromise;
+    }
+
+    previewOutward(divId, viewerConfig) {
+        const config = {
+            clientId: "8de373cceb0744de81c8ce0ab7c9b434",
+        };
+        if (divId) { 
+            config.divId = divId;
+        }
+        this.adobeDCView = new window.AdobeDC.View(config);
+
+        const previewFilePromise = this.adobeDCView.previewFile({
+            content: {
+                location: {
+                    url: OutwardCertificate,
+                },
+            },
+            metaData: {
+                fileName: "dubosewsOutwardBound.pdf",
                 id: "6d07d124-ac85-43b3-a867-36930f502ac6",
             },
         }, {defaultViewMode: "fit", dockPageControls: false}, viewerConfig);
@@ -115,35 +94,25 @@ class ViewSDKClient {
     }
 
     previewFileUsingFilePromise(divId, filePromise, fileName) {
-        /* Initialize the AdobeDC View object */
         this.adobeDCView = new window.AdobeDC.View({
-            /* Pass your registered client id */
-            clientId: "8c0cd670273d451cbc9b351b11d22318",
-            /* Pass the div id in which PDF should be rendered */
+            clientId: "8de373cceb0744de81c8ce0ab7c9b434",
             divId,
         });
 
-        /* Invoke the file preview API on Adobe DC View object */
         this.adobeDCView.previewFile({
-            /* Pass information on how to access the file */
             content: {
-                /* pass file promise which resolve to arrayBuffer */
                 promise: filePromise,
             },
-            /* Pass meta data of file */
             metaData: {
-                /* file name */
                 fileName: fileName
             }
         }, {});
     }
 
     registerSaveApiHandler() {
-        /* Define Save API Handler */
         const saveApiHandler = (metaData, content, options) => {
             console.log(metaData, content, options);
             return new Promise(resolve => {
-                /* Dummy implementation of Save API, replace with your business logic */
                 setTimeout(() => {
                     const response = {
                         code: window.AdobeDC.View.Enum.ApiResponseCode.SUCCESS,
@@ -164,17 +133,12 @@ class ViewSDKClient {
     }
 
     registerEventsHandler() {
-        /* Register the callback to receive the events */
         this.adobeDCView.registerCallback(
-            /* Type of call back */
             window.AdobeDC.View.Enum.CallbackType.EVENT_LISTENER,
-            /* call back function */
             event => {
                 console.log(event);
             },
-            /* options to control the callback execution */
             {
-                /* Enable PDF analytics events on user interaction. */
                 enablePDFAnalytics: true,
             }
         );
