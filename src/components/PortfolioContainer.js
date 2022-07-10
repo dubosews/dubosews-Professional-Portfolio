a7c46caf88035276882acd124c9b272daf4a35bd
Wesley DuBose <wsd10205@gmail.com>
2022-07-09 23:01:52 -0400

    Update PortfolioContainer.js

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -5,12 +5,18 @@ import About from './pages/About';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 import Resume from './pages/Resume';
+import Mobile from './pages/Mobile.js';
+import {isMobile} from 'react-device-detect'
 
 import portHeader from '../img/portfolioHeaderSmall.png';
 import '../css/app.css';
 import '../css/projects.css';
 
 export default function PortfolioContainer() {
+  
+  if (isMobile){
+    
+  }
   const [currentPage, setCurrentPage] = useState('Home');
 
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
@@ -34,9 +40,11 @@ export default function PortfolioContainer() {
   };
 
   const handlePageChange = (page) => setCurrentPage(page);
-
+  if (isMobile){
+    return <Mobile />;
+  }
   return (
-    <div>
+    <div className="desktopBG">
       <header className="header">
           <img src={portHeader} alt="dubosews header" />
       </header>
